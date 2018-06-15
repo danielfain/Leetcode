466624e97d41b3cc43d6b5fbc08aad0a86c9c04c
Daniel Fain <d6niel@protonmail.com>
2018-06-15 19:34:35 -0400

    work

---
+++ easy/addbinary.py
@@ -0,0 +1,18 @@
+def addBinary(a, b):
+	"""
+	:type a: str
+	:type b: str
+	:rtype: str
+	"""
+	a = int(a, 2)
+	b = int(b, 2)
+	return bin(a + b)[2:]
+
+
+
+a = '101'
+b = '1011'
+print(addBinary(a, b))
+	
+
+
